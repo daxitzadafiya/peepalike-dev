38fa9a9b697c0d977c8eeeace7ad6af8ddde7ec3
smit zadfiya <57133028+smitzadfiya@users.noreply.github.com>
2021-09-14 23:48:45 +0530

    Organizor profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-
+    protected $table = "users";
     /**
      * The attributes that are mass assignable.
      *
@@ -42,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+
 }
--- app/UserEvents.php
+++ app/UserEvents.php
@@ -4,18 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class UserEvents extends Model
 {
     protected $table = 'user_events';
 
     protected $fillable = [
-        'id', 'user_id', 'event_name', 'vanue_name', 'event_start_date', 'event_start_time', 'event_end_date', 'event_end_time', 'event_image', 'event_location', 'address', 'city', 'state', 'postal_code', 'country', 'latitude', 'longitude', 'distance', 'description', 'status', 'event_rating' ,'is_trending_event',
+        'id', 'user_id', 'event_name', 'vanue_name', 'event_start_date', 'event_start_time', 'event_end_date', 'event_end_time', 'event_image', 'event_location', 'address', 'city', 'state', 'postal_code', 'country', 'latitude', 'longitude', 'distance', 'description', 'status', 'event_rating' ,'is_trending_event','ecid',
     ];
 
     protected $dates = [
         'created_at',
         'updated_at'
     ];
-
+    protected $guarded = ['*'];
     public $timestamps = true;
+    public function getuserDetails()
+    {
+        return $this->belongsTo('App\User','user_id','id');
+    }
 }
